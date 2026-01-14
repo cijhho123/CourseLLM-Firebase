306ba54e8a1608d9e50bce30367218708f4b2831
Yuval Rays <yuvalzvirays@gmail.com>
2026-01-14 20:15:15 +0200

    added to do, code looks even better removed prisma

--- src/services/memory-service/src/app.module.ts
+++ src/services/memory-service/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { MemoriesModule } from "./modules/memory/memories.module";
 import { LoggerModule } from "./common/logger/logger.module";
-import { HealthModule } from "./health/health.module";
+import { HealthModule } from "./modules/health/health.module";
 
 @Module({
     imports: [
