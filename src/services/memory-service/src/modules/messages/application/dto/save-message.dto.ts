e1066ec28d7f9a2910559f6b9a1c156b1c882b81
Yuval Rays <yuvalzvirays@gmail.com>
2026-01-16 17:08:18 +0200

    fixed save message request

--- src/services/memory-service/src/modules/messages/application/dto/save-message.dto.ts
+++ src/services/memory-service/src/modules/messages/application/dto/save-message.dto.ts
@@ -3,14 +3,6 @@ import { IsString, IsNotEmpty, IsIn } from "class-validator";
 
 export class SaveMessageDto {
     @ApiProperty({
-        description: "Chat ID",
-        example: "chat_xyz789",
-    })
-    @IsString()
-    @IsNotEmpty()
-    chatID: string;
-
-    @ApiProperty({
         description: "Message content",
         example: "Can you explain derivatives?",
     })
